b13d2d5d5bee08c962738ca72b8e6be1d13710cc
Shubham Tiwari <shubhmtiwri00@gmail.com>
2022-11-09 15:08:59 +0530

    Final Commit

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,6 @@ module.exports = {
                     { loader: MiniCssExtractPlugin.loader },
                     'css-loader',
                     'sass-loader',
-                    'postcss-loader',
                 ],
             },
        
